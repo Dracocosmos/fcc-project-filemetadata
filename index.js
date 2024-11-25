aebf7bb0f369e43628d4ff0e19815c25e53dbf43
dracocosmos <124890142+Dracocosmos@users.noreply.github.com>
2024-11-25 09:52:42 +0200

    added multer, added path to return data from file. Passes tests.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, _next) {
+  const r = req.file
 
+  res.json({
+    name: r.originalname,
+    type: r.mimetype,
+    size: r.size
+  })
 
+})
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
